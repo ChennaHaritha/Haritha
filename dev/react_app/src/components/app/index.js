76b8507a2adbd167746e81bd6d0be4dd0ff8f0a1
PolepalleChennaHaritha <polepallechennaharitha@hartha-iMac.local>
2019-01-05 09:16:41 +0530

    React files

---
+++ dev/react_app/src/components/app/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+import View from "./view/index";
+import Table from "./table/index";
+
+import{
+        BrowserRouter as Router,
+        Route,
+        Switch,
+        Redirect,
+}from 'react-router-dom'
+
+const tableValues=[
+    ['101','Tony Stark','Iron Man','Avengers'],
+    ['102','Peter Parker','Spider man','Avengers'],
+    ['103','Bruce Wayne','Bat Man','Justice League']
+];
+
+const tableHeaders=['Id','Name','Alias','Team'];
+
+
+
+class App extends React.Component{
+    state={
+         selectedId:-1,
+         selectedRecord: {}
+    }
+   
+    
+
+    render(){
+        return(
+            <Router>
+                <Switch>
+                   <Route exact path="/list" render={(props)=>{
+                       return <Table 
+                                   values={tableValues} 
+                                   headers={tableHeaders} 
+                                   history={props.history}/>
+                   }}/>
+                   <Route exact path="/view/:id" render={(props)=>{
+                       console.log(props)
+                       const data=tableValues.find(val => val[0] === props.match.params.id)
+                       const newRecord={
+                                        name: data[1],
+                                        alias:data[2],
+                                        team:data[3]
+                                      }
+                       return <View 
+                       name={newRecord.name} 
+                       alias={newRecord.alias}
+                       team={newRecord.team}/>
+                   }}/>
+                
+                  <Redirect to="/list"/>
+                </Switch>
+               
+               
+            </Router>
+
+        );
+
+    }
+ } 
+
+
+
+
+
+export default App;
